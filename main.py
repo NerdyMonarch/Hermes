05c5645285f6d63d7f2cae36da53706acfbf5c60
NerdyMonarch <npandey@etech-nyc.org>
2020-09-14 18:16:53 -0400

    Edited Environment Variables for Release

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-from dotenv import load_dotenv
 import os
 import pycord
 import pygroupme
 
-load_dotenv()
-
-discord_token = os.getenv('DISCORD_TOKEN')
-groupme_token = os.getenv('GROUP_ME_TOKEN')
+discord_token = os.environ['DISCORD_TOKEN']
+groupme_token = os.environ['GROUP_ME_TOKEN']
 
 cord = pycord.PyCord(discord_token, groupme_token)
 cord.run()
